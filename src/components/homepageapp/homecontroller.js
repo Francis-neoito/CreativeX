41897a8fc7ef47abd20d92341b919c8b0e4a86d3
francis-neoito <francis.xavier@neoito.com>
2024-09-23 16:01:09 +0530

    style change

--- src/components/homepageapp/homecontroller.js
+++ src/components/homepageapp/homecontroller.js
@@ -1093,9 +1093,9 @@ const initHomeMainApp = function(){
                     <div id="homeAppBannerLogo">
                         <span id="logoTextX">X</span>
                         <span id="logoTextCreative">CREATIVE</span>
-                        <div id="logoOrg">
+                        <!--<div id="logoOrg">
                             Powered by NEOITO
-                        </div>
+                        </div>-->
                         <table id="bannerOptions">
                             <tr>
                                 <td><div class="bannerOptionsButton" style="">Contact Us</div></td>
@@ -1214,10 +1214,11 @@ const initHomeMainApp = function(){
                     </div>
                     <div id="footerContainer">
                         <div id="footerTitle">
-                            <div style="font-size: 3rem;margin-bottom:5rem">CreativeX</div>
-                            <div style="font-size: 1.5rem;font-family:sans-serif;margin-left:3rem">
-                                General Enquiries <br>
-                                safaldas.g@neoito.com
+                            <div style="font-size: 1.5rem;margin-bottom:2rem">CreativeX</div>
+                            <div style="font-size: 1rem;font-family:sans-serif">
+                                Contact us <br>
+                                francis.xvr@outlook.com<br>
+                                safaldasg@gmail.com
                             </div>
                         </div>
                         
@@ -1246,9 +1247,9 @@ const initHomeMainApp = function(){
                     <div id="homeAppBannerLogo">
                         <span id="logoTextX">X</span>
                         <span id="logoTextCreative">CREATIVE</span>
-                        <div id="logoOrg">
+                        <!--<div id="logoOrg">
                             Powered by NEOITO
-                        </div>
+                        </div>-->
                         <table id="bannerOptions">
                             <tr>
                                 <td><div class="bannerOptionsButton" style="">Contact Us</div></td>
@@ -1373,10 +1374,11 @@ const initHomeMainApp = function(){
                     </div>
                     <div id="footerContainer">
                         <div id="footerTitle">
-                            <div style="font-size: 1.3rem;margin-bottom:2rem">CreativeX</div>
-                            <div style="font-size: 1.2rem;font-family:sans-serif;">
-                                General Enquiries <br>
-                                safaldas.g@neoito.com
+                            <div style="font-size: 1.1rem;margin-bottom:1rem">CreativeX</div>
+                            <div style="font-size: 1rem;font-family:sans-serif;">
+                                Contact us <br>
+                                francis.xvr@outlook.com<br>
+                                safaldasg@gmail.com
                             </div>
                         </div>
                         
